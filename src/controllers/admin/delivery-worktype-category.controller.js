30f7b185d08dc9645d9f333d522ceeed10e9d767
Manish Yadav <m.amarnath.yadav@accenture.com>
2025-05-21 16:56:45 +0530

    implement GET API for screen 1 and screen 2

--- src/controllers/admin/delivery-worktype-category.controller.js
+++ src/controllers/admin/delivery-worktype-category.controller.js
@@ -1,6 +1,18 @@
 import { DeliveryWorkTypeCategory } from "../../models/delivery-worktype-category.model.js";
 import { DeliveryWorkType } from "../../models/master.model.js";
 
+const httpGetTaskTypes = async (req, res) => {
+  try {
+    // Fetch all master work types and populate their delivery work types
+    const data = await DeliveryWorkTypeCategory.find({}, { taskType: 1 });
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error fetching task types work types:", error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
 const httpCreateDeliveryWorkTypeCategory = async (req, res) => {
   try {
     const payload = req.body;
@@ -38,4 +50,4 @@ const httpCreateDeliveryWorkTypeCategory = async (req, res) => {
   }
 };
 
-export { httpCreateDeliveryWorkTypeCategory };
+export { httpGetTaskTypes, httpCreateDeliveryWorkTypeCategory };
