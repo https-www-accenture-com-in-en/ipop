dd23656ab914eec2089570831bcb9f4c2fc4bb00
pramod <pramod.k.ginnaram@accenture.com>
2025-05-22 16:45:44 +0530

    add crud support for projects

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -10,18 +10,27 @@ import {
 } from "../controllers/admin/delivery-worktype-category.controller.js";
 
 import {
-  httpAddCluster,
-  httpAddMasterProject,
-  httpGetCluster,
-  httpGetMasterProject,
-} from "../controllers/admin/cluster.controller.js";
-import { httpAddMetadata, httpGetMetadata } from '../controllers/admin/metadata.controller.js';
+  httpAddMetadata,
+  httpGetMetadata,
+} from "../controllers/admin/metadata.controller.js";
 import {
   httpGetTimeOffCategories,
   httpAddTimeOffCategory,
   httpUpdateTimeOffCategory,
   httpDeleteTimeOffCategory,
 } from "../controllers/admin/timeoff.controller.js";
+import {
+  addMasterProject,
+  addSubProject,
+  deleteMasterProject,
+  deleteSubProject,
+  getAllMasterProjects,
+  getAllSubProjects,
+  getMasterProjectById,
+  getSubProjectById,
+  updateMasterProject,
+  updateSubProject,
+} from "../controllers/admin/project.controller.js";
 
 const adminRouter = express.Router();
 
@@ -37,17 +46,23 @@ adminRouter.post(
   httpCreateDeliveryWorkTypeCategory
 );
 
-//SCREEN 9 FURTHER WORK
-adminRouter.post("/clusters", httpAddCluster);
-adminRouter.get("/clusters", httpGetCluster);
+//SCREEN 4
+adminRouter.post("/ticket-metadata", httpAddMetadata);
+adminRouter.get("/ticket-metadata", httpGetMetadata);
 
-adminRouter.post("/masterprojects", httpAddMasterProject);
-adminRouter.get("/masterprojects", httpGetMasterProject);
+//Master Project
+adminRouter.post("/master-projects", addMasterProject);
+adminRouter.get("/master-projects", getAllMasterProjects);
+adminRouter.get("/master-projects/:id", getMasterProjectById);
+adminRouter.put("/master-projects/:id", updateMasterProject);
+adminRouter.delete("/master-projects/:id", deleteMasterProject);
 
-
-//SCREEN 4 
-adminRouter.post('/ticket-metadata',httpAddMetadata);
-adminRouter.get('/ticket-metadata',httpGetMetadata)
+// SubProject Routes
+adminRouter.post("/sub-projects", addSubProject);
+adminRouter.get("/sub-projects", getAllSubProjects);
+adminRouter.get("/sub-projects/:id", getSubProjectById);
+adminRouter.put("/sub-projects/:id", updateSubProject);
+adminRouter.delete("/sub-projects/:id", deleteSubProject);
 
 adminRouter.get("/timeoff", httpGetTimeOffCategories);
 adminRouter.post("/timeoff", httpAddTimeOffCategory);
