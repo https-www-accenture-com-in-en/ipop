9647fc913604c2ea1d68433e7669ed25f46a3512
pramod <pramod.k.ginnaram@accenture.com>
2025-06-05 22:42:27 +0530

    move app.js to root

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
+import http from "http";
+import connectDB from "./db/dbconfig.js"; // Import the database connection function
+import config from "./src/utils/config.js";
+
 import path from "path";
 import express from "express";
 import cors from "cors";
 // import userRouter from './routes/user.route.js';
-import adminRouter from "./routes/admin.route.js";
-import userRouter from "./routes/user.route.js"; // Uncomment this line if you have a userRouter defined
+import adminRouter from "./src/routes/admin.route.js";
+import userRouter from "./src/routes/user.route.js"; // Uncomment this line if you have a userRouter defined
 import {
   requestLogger,
   unknownEndpoint,
   errorHandler,
-} from "./utils/middleware.js";
+} from "./src/utils/middleware.js";
 
 const app = express();
+const PORT = config.PORT || 5000;
+
 app.use(cors({ origin: "*" })); // Allow all origins for CORS, you can specify your frontend URL here
 app.use(requestLogger);
 app.use(express.json());
@@ -30,5 +36,11 @@ app.get("/*name", (req, res) =>
 
 app.use(unknownEndpoint);
 app.use(errorHandler);
+const server = http.createServer(app);
 
+connectDB();
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 export default app; // Export the app for use in other files (e.g., server.js)
